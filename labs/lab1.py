5d416e1652822b72827a1c38b1043b66dad6a78f
burmaldar <145529505+burmaldar@users.noreply.github.com>
2023-09-27 10:09:16 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,15 @@
+#задание1
 import math
 
 def task1():
   pass
+
+#задание2
+def task2(x):
+    b= -2
+    c= 3
+    return ((c*(x**3)+x)/(b*(x**2)))+(x**(1/3))
+
+
+x=float(input())
+print(task2(x))
