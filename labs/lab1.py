dff2aafc770470528da33e8f1e447f733f3efdab
burmaldar <145529505+burmaldar@users.noreply.github.com>
2023-09-27 11:11:10 +0500

    Update lab1.py

--- labs/lab1.py
+++ labs/lab1.py
@@ -13,3 +13,15 @@ def task2(x):
 
 x=float(input())
 print(task2(x))
+#задание 3
+import math
+def task3():
+    x = float(input())
+    return ((math.sin(x)+math.cos(x))/(math.cos(x)-math.sin(x))*math.tan(x))
+print(task3())
+#задание4
+import math
+g = float(input())
+s = float(input())
+l = ((g**2+((s/math.pi)*(1/2)))**1/2)
+print(l)
